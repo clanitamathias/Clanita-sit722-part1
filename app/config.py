4fa8721de2a92ac8d4853cb1a7f245f9df673c7f
clanitamathias <75661931+clanitamathias@users.noreply.github.com>
2024-09-30 20:29:13 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("postgresql://clanita_sit722_part1_8nso_user:SP8eRKsjZKxIzzx72MOboE9PYOIF7RSL@dpg-crt76s5ds78s73eccdog-a.oregon-postgres.render.com/clanita_sit722_part1_8nso")
 
 settings = Settings()
